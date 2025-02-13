cbfdf5ecadadf02f7d59cf50b0f5ed68deabaa91
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-02-13 11:17:44 +0800

    correcting group ids and adding tests

--- mocks/create_mock_catalog.py
+++ mocks/create_mock_catalog.py
@@ -1,3 +1,45 @@
-# TODO: create a script that will read in the mock catalog and put it in the correct format. 
+"""
+Script that converts the parquet files that we have downloaded from SHARK on pawsey into a format
+that is more accepted by the FoFR group finder. In particular renaming the groupids to be 1..N and
+renaming the column names to the keywords that are looked for in the R code.
+"""
+
+import pandas as pd
 
 
+def rename_groups(data_frame: pd.DataFrame) -> pd.DataFrame:
+    """
+    Renaming the data_frame groups from 1 .. N instead of the stingray naming convention.
+    This is the way that the FoFR finder wants to work.
+    """
+    unique_ids = data_frame["id_group_sky"].unique()
+    isolated_mask = data_frame["id_group_sky"] == -1  # isolated gals in stingray are -1
+    grouped_ids = sorted(unique_ids[unique_ids != -1])
+
+    number_isolated_galaxies = isolated_mask.sum()
+    data_frame.loc[isolated_mask, "id_group_sky"] = range(1, number_isolated_galaxies + 1)
+
+    new_id_mapping = {
+        old_id: new_id
+        for new_id, old_id in enumerate(grouped_ids, start=number_isolated_galaxies + 1)
+    }
+    data_frame["id_group_sky"] = data_frame["id_group_sky"].replace(new_id_mapping)
+    return data_frame
+
+
+def rename_ids_col_names(data_frame: pd.DataFrame) -> pd.DataFrame:
+    """
+    Renames the data_frame columns for galaxy ids and group ids to keep consitent with the FoFR
+    naming conventions which FoFempint looks for when comparing to mock catalogs.
+    """
+    data_frame.rename(columns={"id_group_sky": "GroupID", "id_galaxy_sky": "CATAID"})
+    return data_frame
+
+
+if __name__ == "__main__":
+    INFILE = "gama_mock_data/gama_gals.parquet"
+    OUTFILE = "gama_gals_for_R.parquet"
+    df = pd.read_parquet(INFILE)
+    df = rename_groups(df)
+    df = rename_ids_col_names(df)
+    df.to_parquet(OUTFILE, index=False)
---
+++ mocks/test_create_mock_catalog.py
@@ -0,0 +1,58 @@
+"""
+Testing the create_mock_catalog.py
+"""
+
+from io import BytesIO
+import unittest
+import pandas as pd
+
+from create_mock_catalog import rename_ids_col_names, rename_groups
+
+
+class TestRenameFunctions(unittest.TestCase):
+    """
+    Main testing unit for the create_mock_catalog script.
+    """
+
+    def setUp(self):
+        # Create a test dataframe
+        self.df = pd.DataFrame(
+            {
+                "id_group_sky": [
+                    -1,
+                    -1,
+                    -1,
+                    6292300000001,
+                    6292300000001,
+                    6292300000001,
+                ],
+                "id_galaxy sky": [101, 102, 103, 104, 105, 106],
+            }
+        )
+
+        # Save to a Parquet file in memory
+        self.buffer = BytesIO()
+        self.df.to_parquet(self.buffer)
+        self.buffer.seek(0)
+
+    def test_rename_group_ids(self):
+        """
+        Testing that we rename the group ids correctly.
+        """
+        df_test = pd.read_parquet(self.buffer)
+        df_transformed = rename_groups(df_test)
+        expected_group_ids = [1, 2, 3, 4]
+        self.assertListEqual(df_transformed["GroupID"].unique().tolist(), expected_group_ids)
+
+    def test_rename_column_names(self):
+        """
+        Testing that the colums are renamed correctly.
+        """
+        df_test = pd.read_parquet(self.buffer)
+        df_transformed = rename_ids_col_names(df_test)
+        self.assertIn("GroupID", df_transformed.columns)
+        self.assertIn("CATAID", df_transformed.columns)
+
+
+if __name__ == "__main__":
+    unittest.main()
