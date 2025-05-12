c998ec93bec2afcd1145d5dfe8a94b1dc2be98a2
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-05-12 16:40:12 +0800

    Added the fine-tuned parameters

--- create_group_catalog.R
+++ create_group_catalog.R
@@ -105,14 +105,14 @@ column_names <- colnames(gama)
 gama_ids = gama['UberID']
 data_column_names <- column_names[-1]
 #I'm just assuming 100% completeness and I should have a look at the way Aaron does the completeness stuff.
-optuse=c(0.06, 18, 0, -0.02, 0.63, 9.0000, 1.5000, 12.0000)
+optuse=c(0.06, 36, 0, 0, 0, 9.0000, 1.5000, 12.0000)
 # see if this magdenscale makes a difference optuse[5]
 cat=FoF::FoFempint(
   data=gama, bgal=optuse[1], rgal=optuse[2],
   coscale=T, NNscale=3, precalc=F, halocheck=F, apmaglim=19.65, colnames=data_column_names,
   denfunc=LFswmlfunc, intfunc=RunningDensity_z, intLumfunc=LFswmlintfuncLum,
   useorigind=T, realIDs = T, dust=0,scalemass=1,scaleflux=1,extra=F,
-  circsamp=circsamp,Mmax=1e15, zvDmod = z_to_dmod, Dmodvz = dmod_to_z,
+  circsamp=circsamp, Mmax=1e15, zvDmod = z_to_dmod, Dmodvz = dmod_to_z,
   left=129, right=141, top = 3, bottom = -2, verbose=TRUE)
 
 # writing the group catalog and the galaxy linking table.
