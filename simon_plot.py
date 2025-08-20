333f26e04d418fcf1a2bb86dc5d2ade02dd204bc
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-08-20 17:25:06 +0800

    Making zcuts and adding old group cat to the mass etimate in simon plot.

--- simon_plot.py
+++ simon_plot.py
@@ -19,10 +19,10 @@ df_gal = tbl.to_pandas()
 
 tbl = Table.read(INFILE_GROUP)
 df_group = tbl.to_pandas()
-df_group = df_group[df_group['Nfof'] > 3]
+df_group = df_group[df_group['Nfof'] > 5]
+df_group = df_group[df_group['Zfof'] < 0.2]
 
-df_gal['lum'] = 10**(-0.4 * df_gal['AbsoluteMagR'])
-bcg_ids = df_group['IterCenUberID']
+bcg_ids = df_group['BCGUberID']
 
 bcg_dfs = df_gal[df_gal['UberID'].isin(bcg_ids)]
 
@@ -32,17 +32,63 @@ bcg_dfs = bcg_dfs.merge(df_masses, on='UberID', how='left')
 
 
 fraction = np.array(bcg_dfs['StellarMass_50'])/np.array(df_group['MassA'])
-plt.scatter(np.log10(df_group['MassAfunc']), np.log10(fraction))
-#plt.ylim(0, 1)
-plt.xlim(left=11)
-plt.show()
-
-plt.hist2d(np.log10(df_group['MassAfunc']), np.log10(fraction), bins=100, cmap='plasma')
-plt.xlim(left=11)
-plt.show()
-
-
 start_plot("M_halo", "fraction bcg/halo")
-plt.hexbin(np.log10(df_group['MassAfunc']), np.log10(fraction), gridsize=50, cmap='inferno', mincnt=1)
+plt.hexbin(np.log10(df_group['MassA']), fraction, gridsize=50, cmap='inferno', mincnt=1)
+#plt.xlim(8, 16)
+#plt.ylim(-6, 2)
 end_plot("simon_plot.png")
 plt.close()
+
+
+"""
+#Trying the same analysis except using the old data.
+"""
+
+tbl = Table.read("/Users/00115372/Desktop/GAMA_paper_plotter/old_gama_data/ProSpectv03.fits")
+df_prospect = tbl.to_pandas()
+
+tbl = Table.read("/Users/00115372/Desktop/GAMA_paper_plotter/old_gama_data/G3CFoFGroupv10.fits")
+df_groups_10 = tbl.to_pandas()
+df_groups_10 = df_groups_10[df_groups_10['Nfof'] > 5]
+df_groups_10 = df_groups_10[df_groups_10['Zfof'] < 0.2]
+
+bcg_ids = np.array(df_groups_10['BCGCATAID'])
+
+all_ids = np.array(df_prospect['CATAID'])
+idx = []
+bad_group_idx = []
+for i, _id in enumerate(bcg_ids):
+    _idx = np.where(all_ids == _id)[0]
+    if len(_idx) != 0:
+        idx.append(_idx[0])
+    else:
+        bad_group_idx.append(i)
+bad_group_idx = np.array(bad_group_idx)
+good_group_idx = np.setdiff1d(np.arange(len(df_groups_10)), bad_group_idx)
+idx = np.array(idx)
+
+bad_groups = df_groups_10.iloc[bad_group_idx]
+df_groups_10 = df_groups_10.iloc[good_group_idx]
+
+stellar_mass_bcg = np.array(df_prospect.iloc[idx]['StellarMass_50'])
+halo_mass_a = np.array(df_groups_10['MassA'])
+halo_mass_afunc = np.array(df_groups_10['MassAfunc'])
+
+start_plot("Halo Mass [Msol]", "Fraction of masss in BCG [percent]")
+plt.hexbin(np.log10(halo_mass_a), stellar_mass_bcg/halo_mass_a, gridsize=50, cmap='inferno', mincnt=1)
+#plt.xlim(8, 16)
+#plt.ylim(-6, 2)
+end_plot("simon_plot_old_group_cat.png")
+plt.close()
+
+
+"""
+Working out the same plot but for Shark
+"""
+
+#INFILE_SHARK = "/Users/00115372/Desktop/mock_catalogs/offical_waves_mocks/v0.3.1/waves_wide_gals.parquet"
+#df_shark = pd.read_parquet(INFILE_SHARK)
+
+
+# TODO: Shark needs the Type included for me to work this trend out directly from simulations.
+# TODO: Once shark has worked that out then we can run Nesie on Shark and see how that differs.
